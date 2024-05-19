55724e52dc52905a45fe5652ac911e01a69a5421
PeaceNaza <peacedonatus38@gmail.com>
2024-05-19 20:14:33 +0100

    feat: add new task

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useMemo } from "react"
+//import { useMemo } from "react"
 import { useStore } from "../store"
 import Task from "./Task"
 
@@ -12,12 +12,16 @@ function Column({ state }) {
 // const filtered = useMemo(() => tasks.filter((task) => task.state === state), [tasks, state])
 
   return (
+
     <div className="bg-primary shadow-lg min-h-[20rem] w-[33%] max-w-[20rem] my-0 mx-[0.5rem] rounded-md p-3 mt-5">
-    <p>{state}</p>  
-    {/**for each task return the task component */}
+      <div className="flex justify-between items-center">
+         <p>{state}</p>  
+         <button className="bg-white mb-2 py-1 px-2 rounded-md h-fit hover:bg-gray-300 cursor-pointer">Add</button>
+      </div>
+    {/**for each task return the task component */} 
     {tasks.map((task) => (
        <Task title={task.title} key={task.title}/>
-    ))}
+    ))} 
     </div>
 
   )
--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
 import { create } from "zustand"
 
 const store = (set) => ({
-  tasks: [
-    {
-      title: "TestTask", 
-      state: "ONGOING",
-    }
-  ]
+
+  tasks: [{title: "TestTask", state: "ONGOING"}], //array of tasks
+
+  addTask: (title, state) => set((store) => ({tasks: [...store.tasks, {title, state}]})), //adds a task to the tasks array
+
 });
 
 export const useStore = create(store); 
+
 
